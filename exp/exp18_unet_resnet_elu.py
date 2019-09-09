1c5db4272710b88bb25fc8c1c7c3e04d016369f4
okotaku <to78314910@gmail.com>
2019-09-09 23:48:22 +0900

    add exp18

--- exp/exp18_unet_resnet_elu.py
+++ exp/exp18_unet_resnet_elu.py
@@ -53,8 +53,8 @@ EPOCHS = 101
 FOLD_ID = 0
 EXP_ID = "exp18_unet_resnet"
 base_ckpt = 0
-#base_model = None
-base_model = "models/{}_fold{}_ckpt{}.pth".format(EXP_ID, FOLD_ID, base_ckpt)
+base_model = None
+#base_model = "models/{}_fold{}_ckpt{}.pth".format(EXP_ID, FOLD_ID, base_ckpt)
 
 setup_logger(out_file=LOGGER_PATH)
 seed_torch(SEED)
