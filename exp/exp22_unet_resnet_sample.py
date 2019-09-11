bc452c181bc832d91300fbf13fc3c14c5d8bc217
takumi-okoshi <takumi.okoshi@dena.com>
2019-09-11 15:12:32 +0900

    add exp22

--- exp/exp22_unet_resnet_sample.py
+++ exp/exp22_unet_resnet_sample.py
@@ -51,12 +51,12 @@ device = "cuda:0"
 IMG_SIZE = (800, 128)
 CLR_CYCLE = 3
 BATCH_SIZE = 128
-EPOCHS = 83
+EPOCHS = 47
 FOLD_ID = 0
 EXP_ID = "exp22_unet_resnet_check"
-base_ckpt = 0
-base_model = None
-#base_model = "models/{}_fold{}_ckpt{}.pth".format(EXP_ID, FOLD_ID, base_ckpt)
+base_ckpt = 6
+#base_model = None
+base_model = "models/{}_fold{}_ckpt{}.pth".format(EXP_ID, FOLD_ID, base_ckpt)
 
 setup_logger(out_file=LOGGER_PATH)
 seed_torch(SEED)
