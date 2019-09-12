648e1be4c770840e6ad69c1fe2c7e08e4fc3764c
takumi-okoshi <takumi.okoshi@dena.com>
2019-09-13 07:15:14 +0900

    add exp26

--- exp/exp26_unet_seresnext50.py
+++ exp/exp26_unet_seresnext50.py
@@ -50,7 +50,7 @@ accumulation_steps = 4
 EPOCHS = 83
 FOLD_ID = 0
 EXP_ID = "exp26_unet_seresnext50"
-base_ckpt = 2
+base_ckpt = 5
 base_model = None
 #base_model = "models/{}_fold{}_ckpt{}.pth".format(EXP_ID, FOLD_ID, base_ckpt)
 base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
@@ -132,7 +132,7 @@ def main(seed):
         best_model_ep = 0
         checkpoint = base_ckpt+1
 
-        for epoch in range(13, EPOCHS + 1):
+        for epoch in range(32, EPOCHS + 1):
             seed = seed + epoch
             seed_torch(seed)
             if epoch % (CLR_CYCLE * 2) == 0:
