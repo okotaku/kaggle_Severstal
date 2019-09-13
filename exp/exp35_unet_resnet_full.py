49fb13850c2da3692b9961fa9ec6bc0ed0f20517
okotaku <to78314910@gmail.com>
2019-09-13 22:50:17 +0900

    add exp35

--- exp/exp35_unet_resnet_full.py
+++ exp/exp35_unet_resnet_full.py
@@ -142,6 +142,7 @@ def main(seed):
             seed = seed + epoch
             seed_torch(seed)
             if epoch % (CLR_CYCLE * 2) == 0:
+                torch.save(model.module.state_dict(), 'models/{}_fold{}_latest.pth'.format(EXP_ID, FOLD_ID))
                 LOGGER.info('Best valid loss: {} on epoch={}'.format(round(best_model_loss, 5), best_model_ep))
                 checkpoint += 1
                 best_model_loss = 999
@@ -163,8 +164,6 @@ def main(seed):
                 best_model_loss = valid_loss
                 best_model_ep = epoch
                 #np.save("val_pred.npy", val_pred)
-
-            torch.save(model.module.state_dict(), 'models/{}_fold{}_latest.pth'.format(EXP_ID, FOLD_ID))
 
             #del val_pred
             gc.collect()
