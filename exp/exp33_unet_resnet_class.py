7adb907c6766c13ba028d87bb039c54e3b287651
okotaku <to78314910@gmail.com>
2019-09-13 20:45:57 +0900

    add exp33

--- exp/exp33_unet_resnet_class.py
+++ exp/exp33_unet_resnet_class.py
@@ -134,7 +134,7 @@ def main(seed):
         best_model_ep = 0
         checkpoint = base_ckpt+1
 
-        for epoch in range(67, EPOCHS + 1):
+        for epoch in range(66, EPOCHS + 1):
             seed = seed + epoch
             seed_torch(seed)
             if epoch % (CLR_CYCLE * 2) == 0:
