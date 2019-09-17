3f9eb42c1643cbaff54ad40c7692276b6cfb55a7
takumi-okoshi <takumi.okoshi@dena.com>
2019-09-17 11:23:00 +0900

    add panet

--- segmentation_models_pytorch/__init__.py
+++ segmentation_models_pytorch/__init__.py
@@ -5,6 +5,7 @@ from .pspnet import PSPNet
 from .unetpp import UnetPP
 from .linknetpp import LinknetPP
 from .efunet import EfficientNet_3_unet, EfficientNet_5_unet
+from .panet import PANet
 
 from . import encoders
 from . import utils
