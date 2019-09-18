5a7a3338403683f03bc0ab16786002eabfb67a80
okotaku <to78314910@gmail.com>
2019-09-18 23:31:44 +0900

    53

--- exp/exp53_fix_decodersize.py
+++ exp/exp53_fix_decodersize.py
@@ -48,16 +48,16 @@ IMG_SIZE = (1600, 256)
 CLR_CYCLE = 3
 BATCH_SIZE = 16
 EPOCHS = 101
-FOLD_ID = 0
+FOLD_ID = 1
 EXP_ID = "exp53_unet_resnet"
 CLASSIFICATION = True
 EMA = True
 EMA_START = 6
-base_ckpt = 2
+base_ckpt = 0
 base_model = None
 base_model_ema = None
-base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
-base_model_ema = "models/{}_fold{}_latest_ema.pth".format(EXP_ID, FOLD_ID)
+#base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
+#base_model_ema = "models/{}_fold{}_latest_ema.pth".format(EXP_ID, FOLD_ID)
 
 setup_logger(out_file=LOGGER_PATH)
 seed_torch(SEED)
@@ -158,7 +158,7 @@ def main(seed):
         ema_decay = 0
         checkpoint = base_ckpt+1
 
-        for epoch in range(12, EPOCHS + 1):
+        for epoch in range(1, EPOCHS + 1):
             seed = seed + epoch
             seed_torch(seed)
 
