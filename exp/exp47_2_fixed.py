7b224342c7c0b76e60ef2ede5a903d373df588b3
takumi-okoshi <takumi.okoshi@dena.com>
2019-09-18 10:09:04 +0900

    47

--- exp/exp47_2_fixed.py
+++ exp/exp47_2_fixed.py
@@ -44,7 +44,7 @@ N_CLASSES = 4
 # ===============
 SEED = np.random.randint(100000)
 device = "cuda:0"
-IMG_SIZE = (1600, 256)
+IMG_SIZE = (800, 128)
 CLR_CYCLE = 3
 BATCH_SIZE = 32
 EPOCHS = 119 #101
