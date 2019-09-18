d9a4b60ff39d3a658f9309be2cac0a4d615c9327
takumi-okoshi <takumi.okoshi@dena.com>
2019-09-18 13:41:46 +0900

    48

--- exp/exp48_efunet.py
+++ exp/exp48_efunet.py
@@ -52,9 +52,9 @@ EPOCHS = 83
 FOLD_ID = 0
 EXP_ID = "exp22_unet_resnet"
 CLASSIFICATION = True
-base_ckpt = 0
+base_ckpt = 4
 base_model = None
-#base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
+base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
 
 setup_logger(out_file=LOGGER_PATH)
 seed_torch(SEED)
@@ -141,7 +141,7 @@ def main(seed):
         best_model_ep = 0
         checkpoint = base_ckpt+1
 
-        for epoch in range(1, EPOCHS + 1):
+        for epoch in range(24, EPOCHS + 1):
             seed = seed + epoch
             seed_torch(seed)
 
