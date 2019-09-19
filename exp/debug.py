d2173d4484267c649150ec947e59d1a7688d1d42
okotaku <to78314910@gmail.com>
2019-09-20 08:19:21 +0900

    check nan

---
+++ exp/debug.py
@@ -0,0 +1,278 @@
+# ===============
+# best_ckpt=
+# ===============
+import os
+import gc
+import copy
+import time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#import segmentation_models_pytorch as smp
+from apex import amp
+from contextlib import contextmanager
+from albumentations import *
+import torch
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+import sys
+sys.path.append("../severstal-src/")
+from util import seed_torch, search_threshold
+from losses import FocalLovaszLoss
+from datasets import SeverDataset, MaskProbSampler
+from logger import setup_logger, LOGGER
+from trainer import train_one_epoch, validate
+from scheduler import GradualWarmupScheduler
+sys.path.append("../")
+import segmentation_models_pytorch as smp
+from sync_batchnorm import convert_model
+
+
+# ===============
+# Constants
+# ===============
+IMG_DIR = "../input/train_images/"
+LOGGER_PATH = "log.txt"
+FOLD_PATH = "../input/severstal_folds01.csv"
+ID_COLUMNS = "ImageId"
+N_CLASSES = 4
+
+
+# ===============
+# Settings
+# ===============
+SEED = np.random.randint(100000)
+device = "cuda"
+IMG_SIZE = (1600, 256)
+CLR_CYCLE = 3
+BATCH_SIZE = 16
+EPOCHS = 101
+FOLD_ID = 0
+EXP_ID = "exp56_unet_seresnext"
+CLASSIFICATION = True
+EMA = True
+EMA_START = 6
+base_ckpt = 3
+base_model = None
+base_model_ema = None
+base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
+base_model_ema = "models/{}_fold{}_latest_ema.pth".format(EXP_ID, FOLD_ID)
+
+setup_logger(out_file=LOGGER_PATH)
+seed_torch(SEED)
+LOGGER.info("seed={}".format(SEED))
+
+
+@contextmanager
+def timer(name):
+    t0 = time.time()
+    yield
+    LOGGER.info('[{}] done in {} s'.format(name, round(time.time() - t0, 2)))
+
+import os
+import cv2
+import torch
+import random
+import pydicom
+import numpy as np
+from torch.utils.data import Dataset
+from torch.utils.data.sampler import Sampler
+
+from util import rle2mask
+from logger import LOGGER
+
+
+class SeverDataset(Dataset):
+
+    def __init__(self,
+                 df,
+                 img_dir,
+                 img_size,
+                 n_classes,
+                 crop_rate=1.0,
+                 id_colname="ImageId",
+                 mask_colname=["EncodedPixels_{}".format(i) for i in range(1, 5)],
+                 transforms=None,
+                 means=[0.485, 0.456, 0.406],
+                 stds=[0.229, 0.224, 0.225],
+                 class_y=None,
+                 cut_h=False,
+                 crop_320=False
+                 ):
+        self.df = df
+        self.img_dir = img_dir
+        self.img_size = img_size
+        self.transforms = transforms
+        self.means = np.array(means)
+        self.stds = np.array(stds)
+        self.id_colname = id_colname
+        self.mask_colname = mask_colname
+        self.n_classes = n_classes
+        self.crop_rate = crop_rate
+        self.class_y = class_y
+        self.cut_h = cut_h
+        self.crop_320 = crop_320
+
+    def __len__(self):
+        return self.df.shape[0]
+
+    def __getitem__(self, idx):
+        cur_idx_row = self.df.iloc[idx]
+        img_id = cur_idx_row[self.id_colname]
+        img_path = os.path.join(self.img_dir, img_id)
+
+        img = cv2.imread(img_path)
+        w, h, _ = img.shape
+        mask = np.zeros((w, h, self.n_classes))
+        for i, encoded in enumerate(cur_idx_row[self.mask_colname]):
+            if encoded in "-1":
+                continue
+            else:
+                mask[:, :, i] = rle2mask(encoded, (w, h))
+
+        if self.crop_rate < 1:
+            img, mask = random_wcropping(img, mask, is_random=True, ratio=self.crop_rate)
+        if self.crop_320:
+            img, mask = random_320cropping(img, mask)
+        img = cv2.resize(img, self.img_size)
+        mask = cv2.resize(mask, self.img_size)
+        mask[mask != 0] = 1
+
+        if self.transforms is not None:
+            augmented = self.transforms(image=img, mask=mask)
+            img = augmented['image']
+            mask = augmented['mask']
+        if self.cut_h:
+            img, mask = cutout_h(img, mask, self.img_size, self.n_classes)
+
+        img = img / 255
+        img -= self.means
+        img /= self.stds
+        img = img.transpose((2, 0, 1))
+        mask = mask.transpose((2, 0, 1))
+        mask = torch.from_numpy(mask)
+
+        if self.class_y is not None:
+            class_y_ = self.class_y[idx]
+            target = {"mask": mask, "class_y": torch.tensor(class_y_)}
+        else:
+            target = mask
+
+        return torch.from_numpy(img), target, img_id
+
+def validate(model, valid_loader, criterion, device, classification=False):
+    model.eval()
+    test_loss = 0.0
+    true_ans_list = []
+    preds_cat = []
+    with torch.no_grad():
+
+        for step, (features, targets, ids) in enumerate(valid_loader):
+            features, targets = features.to(device), targets.to(device)
+            print(ids)
+
+            if classification:
+                logits, _ = model(features)
+                loss = criterion(logits, targets)
+            else:
+                logits = model(features)
+                loss = criterion(logits, targets)
+
+            test_loss += loss.item()
+            print(loss.item())
+            #true_ans_list.append(targets.float().cpu().numpy().astype("int8"))
+            #preds_cat.append(torch.sigmoid(logits).float().cpu().numpy().astype("float16"))
+
+            del features, targets, logits
+            gc.collect()
+
+        #all_true_ans = np.concatenate(true_ans_list, axis=0)
+        #all_preds = np.concatenate(preds_cat, axis=0)
+
+    return test_loss / (step + 1)#, all_preds, all_true_ans
+
+
+def main(seed):
+    with timer('load data'):
+        df = pd.read_csv(FOLD_PATH)
+        y1 = (df.EncodedPixels_1 != "-1").astype("float32").values.reshape(-1, 1)
+        y2 = (df.EncodedPixels_2 != "-1").astype("float32").values.reshape(-1, 1)
+        y3 = (df.EncodedPixels_3 != "-1").astype("float32").values.reshape(-1, 1)
+        y4 = (df.EncodedPixels_4 != "-1").astype("float32").values.reshape(-1, 1)
+        y = np.concatenate([y1, y2, y3, y4], axis=1)
+
+    with timer('preprocessing'):
+        train_df, val_df = df[df.fold_id != FOLD_ID], df[df.fold_id == FOLD_ID]
+        y_train, y_val = y[df.fold_id != FOLD_ID], y[df.fold_id == FOLD_ID]
+
+        train_augmentation = Compose([
+            Flip(p=0.5),
+            OneOf([
+                GridDistortion(p=0.5),
+                OpticalDistortion(p=0.5, distort_limit=2, shift_limit=0.5)
+            ], p=0.5),
+            OneOf([
+                RandomGamma(gamma_limit=(100,140), p=0.5),
+                RandomBrightnessContrast(p=0.5),
+                RandomBrightness(p=0.5),
+                RandomContrast(p=0.5)
+            ], p=0.5),
+            OneOf([
+                GaussNoise(p=0.5),
+                Cutout(num_holes=10, max_h_size=10, max_w_size=20, p=0.5)
+            ], p=0.5),
+            ShiftScaleRotate(rotate_limit=20, p=0.5),
+        ])
+        val_augmentation = None
+
+        train_dataset = SeverDataset(train_df, IMG_DIR, IMG_SIZE, N_CLASSES, id_colname=ID_COLUMNS,
+                                    transforms=train_augmentation, crop_rate=1.0, class_y=y_train)
+        val_dataset = SeverDataset(val_df, IMG_DIR, IMG_SIZE, N_CLASSES, id_colname=ID_COLUMNS,
+                                  transforms=val_augmentation)
+        train_sampler = MaskProbSampler(train_df, demand_non_empty_proba=0.6)
+        train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, sampler=train_sampler, num_workers=8)
+        val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=8)
+
+        del train_df, val_df, df, train_dataset, val_dataset
+        gc.collect()
+
+    with timer('create model'):
+        model = smp.Unet('se_resnext50_32x4d', encoder_weights="imagenet", classes=N_CLASSES, encoder_se_module=True,
+                         decoder_semodule=True, h_columns=False, skip=True, act="swish", freeze_bn=True,
+                         classification=CLASSIFICATION, attention_type="cbam", center=True)
+        model = convert_model(model)
+        if base_model is not None:
+            model.load_state_dict(torch.load(base_model))
+        model.to(device)
+
+        criterion = torch.nn.BCEWithLogitsLoss()
+        optimizer = torch.optim.Adam([
+            {'params': model.decoder.parameters(), 'lr': 3e-3},
+            {'params': model.encoder.parameters(), 'lr': 3e-4},
+        ])
+        if base_model is None:
+            scheduler_cosine = CosineAnnealingLR(optimizer, T_max=CLR_CYCLE, eta_min=3e-5)
+            scheduler = GradualWarmupScheduler(optimizer, multiplier=1.1, total_epoch=CLR_CYCLE*2, after_scheduler=scheduler_cosine)
+        else:
+            scheduler = CosineAnnealingLR(optimizer, T_max=CLR_CYCLE, eta_min=3e-5)
+
+
+        model, optimizer = amp.initialize(model, optimizer, opt_level="O1", verbosity=0)
+
+        if EMA:
+            ema_model = copy.deepcopy(model)
+            if base_model_ema is not None:
+                ema_model.load_state_dict(torch.load(base_model_ema))
+            ema_model.to(device)
+        else:
+            ema_model = None
+        model = torch.nn.DataParallel(model)
+        ema_model = torch.nn.DataParallel(ema_model)
+
+    with timer('train'):
+        valid_loss = validate(model, val_loader, criterion, device, classification=CLASSIFICATION)
+
+
+if __name__ == '__main__':
+    main(SEED)
