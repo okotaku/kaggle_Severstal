530d08de55922e87021c8d44678241642d4fc58a
okotaku <to78314910@gmail.com>
2019-09-20 08:38:19 +0900

    53

--- exp/exp53_fix_decodersize.py
+++ exp/exp53_fix_decodersize.py
@@ -158,7 +158,7 @@ def main(seed):
         optimizer = torch.optim.Adam([
             {'params': model.decoder.parameters(), 'lr': 3e-3},
             {'params': model.encoder.parameters(), 'lr': 3e-4},
-        ])
+        ], eps=1e-4)
         if base_model is None:
             scheduler_cosine = CosineAnnealingLR(optimizer, T_max=CLR_CYCLE, eta_min=3e-5)
             scheduler = GradualWarmupScheduler(optimizer, multiplier=1.1, total_epoch=CLR_CYCLE*2, after_scheduler=scheduler_cosine)
