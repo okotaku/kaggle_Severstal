e59834986336e69b6ab8fbe03bc8405a9db262b3
okotaku <to78314910@gmail.com>
2019-09-22 22:26:02 +0900

    57

--- exp_cls/exp1_resnet50.py
+++ exp_cls/exp1_resnet50.py
@@ -73,11 +73,12 @@ def timer(name):
 def main(seed):
     with timer('load data'):
         df = pd.read_csv(FOLD_PATH)
-        y1 = (df.EncodedPixels_1 != "-1").astype("float32").values.reshape(-1, 1)
-        y2 = (df.EncodedPixels_2 != "-1").astype("float32").values.reshape(-1, 1)
-        y3 = (df.EncodedPixels_3 != "-1").astype("float32").values.reshape(-1, 1)
-        y4 = (df.EncodedPixels_4 != "-1").astype("float32").values.reshape(-1, 1)
-        y = np.concatenate([y1, y2, y3, y4], axis=1)
+        #y1 = (df.EncodedPixels_1 != "-1").astype("float32").values.reshape(-1, 1)
+        #y2 = (df.EncodedPixels_2 != "-1").astype("float32").values.reshape(-1, 1)
+        #y3 = (df.EncodedPixels_3 != "-1").astype("float32").values.reshape(-1, 1)
+        #y4 = (df.EncodedPixels_4 != "-1").astype("float32").values.reshape(-1, 1)
+        #y = np.concatenate([y1, y2, y3, y4], axis=1)
+        y = (df.sum_target != 0).astype("float32").values
 
     with timer('preprocessing'):
         train_df, val_df = df[df.fold_id != FOLD_ID], df[df.fold_id == FOLD_ID]
