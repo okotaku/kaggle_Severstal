efb2113e966dcd42f5bf58eaeaf8315d437c73d1
takumi-okoshi <takumi.okoshi@dena.com>
2019-09-24 13:22:05 +0900

    59

--- severstal-src/datasets.py
+++ severstal-src/datasets.py
@@ -79,7 +79,7 @@ class SeverDataset(Dataset):
         img /= self.stds
         img = img.transpose((2, 0, 1))
         mask = mask.transpose((2, 0, 1))
-        mask = torch.from_numpy(mask)
+        mask = torch.tensor(mask)
 
         if self.class_y is not None:
             class_y_ = self.class_y[idx]
@@ -87,7 +87,7 @@ class SeverDataset(Dataset):
         else:
             target = mask
 
-        return torch.from_numpy(img), target
+        return torch.tensor(img), target
 
 
 class SeverCLSDataset(Dataset):
