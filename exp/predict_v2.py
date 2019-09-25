3e7b53bec66e1ca568d831181e2aa1ffb0578abd
takumi-okoshi <takumi.okoshi@dena.com>
2019-09-25 21:33:08 +0900

    57

--- exp/predict_v2.py
+++ exp/predict_v2.py
@@ -149,7 +149,7 @@ def main(seed):
         criterion = torch.nn.BCEWithLogitsLoss()
 
     with timer('predict'):
-        valid_loss, y_pred, y_true = predict(model, val_loader, criterion, device, classification=CLASSIFICATION)
+        valid_loss, y_pred, y_true, cls = predict(model, val_loader, criterion, device, classification=CLASSIFICATION)
         LOGGER.info('Mean valid loss: {}'.format(round(valid_loss, 5)))
 
         scores = []
