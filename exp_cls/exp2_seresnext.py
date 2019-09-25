4e5264bbf4919f7bd591f0b555e5c6545df29bf2
takumi-okoshi <takumi.okoshi@dena.com>
2019-09-25 21:55:29 +0900

    2

--- exp_cls/exp2_seresnext.py
+++ exp_cls/exp2_seresnext.py
@@ -56,15 +56,15 @@ IMG_SIZE = (1600, 256)
 CLR_CYCLE = 3
 BATCH_SIZE = 32
 EPOCHS = 59
-FOLD_ID = 4
+FOLD_ID = 2
 EXP_ID = "cls_exp2_seresnext"
 EMA = True
 EMA_START = 6
-base_ckpt = 0
+base_ckpt = 5
 base_model = None
 base_model_ema = None
-#base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
-#base_model_ema = "models/{}_fold{}_latest_ema.pth".format(EXP_ID, FOLD_ID)
+base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
+base_model_ema = "models/{}_fold{}_latest_ema.pth".format(EXP_ID, FOLD_ID)
 
 setup_logger(out_file=LOGGER_PATH)
 seed_torch(SEED)
@@ -161,7 +161,7 @@ def main(seed):
         ema_decay = 0
         checkpoint = base_ckpt+1
 
-        for epoch in range(1, EPOCHS + 1):
+        for epoch in range(30, EPOCHS + 1):
             seed = seed + epoch
             seed_torch(seed)
 
