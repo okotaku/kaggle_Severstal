a8ac35cbdfc4f4979f348c6911931de6470cf6c7
takumi-okoshi <takumi.okoshi@dena.com>
2019-09-26 13:50:21 +0900

    67

--- exp/exp67_unet_seres_lr.py
+++ exp/exp67_unet_seres_lr.py
@@ -53,15 +53,15 @@ CLR_CYCLE = 3
 BATCH_SIZE = 16
 EPOCHS = 101
 FOLD_ID = 0
-EXP_ID = "exp62_unet_seres"
+EXP_ID = "exp67_unet_seres"
 CLASSIFICATION = True
 EMA = True
 EMA_START = 6
-base_ckpt = 0
+base_ckpt = 2
 base_model = None
 base_model_ema = None
-#base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
-#base_model_ema = "models/{}_fold{}_latest_ema.pth".format(EXP_ID, FOLD_ID)
+base_model = "models/{}_fold{}_latest.pth".format("exp62_unet_seres", FOLD_ID)
+base_model_ema = "models/{}_fold{}_latest_ema.pth".format("exp62_unet_seres", FOLD_ID)
 
 setup_logger(out_file=LOGGER_PATH)
 seed_torch(SEED)
@@ -162,7 +162,7 @@ def main(seed):
         ema_decay = 0
         checkpoint = base_ckpt+1
 
-        for epoch in range(1, EPOCHS + 1):
+        for epoch in range(12, EPOCHS + 1):
             seed = seed + epoch
             seed_torch(seed)
 
