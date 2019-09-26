ecd2b09993a22c377bcd55ff54714541936af401
okotaku <to78314910@gmail.com>
2019-09-26 19:34:42 +0900

    71

--- exp/exp71_unetpp_resnet_4cls.py
+++ exp/exp71_unetpp_resnet_4cls.py
@@ -50,9 +50,9 @@ EPOCHS = 101 #101
 FOLD_ID = 3
 EXP_ID = "exp71_unetpp_resnet"
 CLASSIFICATION = True
-base_ckpt = 0
+base_ckpt = 2
 base_model = None
-#base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
+base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
 
 setup_logger(out_file=LOGGER_PATH)
 seed_torch(SEED)
@@ -142,7 +142,7 @@ def main(seed):
         best_model_ep = 0
         checkpoint = base_ckpt+1
 
-        for epoch in range(1, EPOCHS + 1):
+        for epoch in range(12, EPOCHS + 1):
             seed = seed + epoch
             seed_torch(seed)
 
