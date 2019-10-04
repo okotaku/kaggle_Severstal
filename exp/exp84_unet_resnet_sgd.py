230c006240a064ef01bc321a65c9115d57ab43bb
takumi-okoshi <takumi.okoshi@dena.com>
2019-10-04 16:15:48 +0900

    exp84

--- exp/exp84_unet_resnet_sgd.py
+++ exp/exp84_unet_resnet_sgd.py
@@ -51,9 +51,9 @@ FOLD_ID = 0
 GAMMA = None
 EXP_ID = "exp84_unet_seresnext"
 CLASSIFICATION = True
-base_ckpt = 0
+base_ckpt = 5
 base_model = None
-#base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
+base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
 
 setup_logger(out_file=LOGGER_PATH)
 seed_torch(SEED)
@@ -140,7 +140,7 @@ def main(seed):
         best_model_score = 0
         checkpoint = base_ckpt+1
 
-        for epoch in range(1, EPOCHS + 1):
+        for epoch in range(30, EPOCHS + 1):
             seed = seed + epoch
             seed_torch(seed)
 
