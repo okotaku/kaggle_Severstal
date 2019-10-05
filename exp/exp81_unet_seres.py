46f9543fc20d202c644cded7251e0f3e8578c5c0
takumi-okoshi <takumi.okoshi@dena.com>
2019-10-05 23:09:14 +0900

    81

--- exp/exp81_unet_seres.py
+++ exp/exp81_unet_seres.py
@@ -54,13 +54,13 @@ IMG_SIZE = (1600, 256)
 CLR_CYCLE = 3
 BATCH_SIZE = 16
 EPOCHS = 137
-FOLD_ID = 0
+FOLD_ID = 1
 GAMMA = None
 EXP_ID = "exp81_unet_seresnext"
 CLASSIFICATION = True
-base_ckpt = 13
+base_ckpt = 10
 base_model = None
-#base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
+base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
 
 setup_logger(out_file=LOGGER_PATH)
 seed_torch(SEED)
@@ -148,7 +148,7 @@ def main(seed):
         best_model_score = 0
         checkpoint = base_ckpt+1
 
-        for epoch in range(78, EPOCHS + 1):
+        for epoch in range(60, EPOCHS + 1):
             seed = seed + epoch
             seed_torch(seed)
 
