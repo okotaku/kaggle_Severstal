2271b51afeaacbf22778a31b3a17e49e8d224a1c
takumi-okoshi <takumi.okoshi@dena.com>
2019-10-10 22:31:13 +0900

    90

---
+++ exp/exp90_fpn_seres.py
@@ -0,0 +1,136 @@
+# ===============
+#
+# ===============
+import os
+import gc
+import time
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+#import segmentation_models_pytorch as smp
+from apex import amp
+from contextlib import contextmanager
+from albumentations import *
+import torch
+from torch.utils.data import DataLoader
+from torch.optim.lr_scheduler import CosineAnnealingLR
+
+import sys
+sys.path.append("../severstal-src/")
+from util import seed_torch, search_threshold
+from losses import FocalLovaszLoss
+from datasets import SeverDataset, MaskProbSampler
+from logger import setup_logger, LOGGER
+from trainer import train_one_epoch, validate
+from scheduler import GradualWarmupScheduler
+sys.path.append("../")
+import segmentation_models_pytorch as smp
+from sync_batchnorm import convert_model
+
+
+# ===============
+# Constants
+# ===============
+IMG_DIR = "../input/train_images/"
+LOGGER_PATH = "log.txt"
+FOLD_PATH = "../input/severstal_folds01.csv"
+ID_COLUMNS = "ImageId"
+N_CLASSES = 4
+
+
+# ===============
+# Settings
+# ===============
+SEED = np.random.randint(100000)
+device = "cuda:0"
+IMG_SIZE = (1600, 256)
+CLR_CYCLE = 3
+BATCH_SIZE = 16
+EPOCHS = 100
+EXP_ID = "exp90_fpn_seresnext_all"
+CLASSIFICATION = False
+base_ckpt = 0
+#base_model = None
+base_model = "models/{}_latest.pth".format(EXP_ID)
+
+setup_logger(out_file=LOGGER_PATH)
+seed_torch(SEED)
+LOGGER.info("seed={}".format(SEED))
+
+
+@contextmanager
+def timer(name):
+    t0 = time.time()
+    yield
+    LOGGER.info('[{}] done in {} s'.format(name, round(time.time() - t0, 2)))
+
+
+def main(seed):
+    with timer('load data'):
+        df = pd.read_csv(FOLD_PATH)
+        y = (df.sum_target != 0).astype("float32").values
+
+    with timer('preprocessing'):
+        train_augmentation = Compose([
+            Flip(p=0.5),
+            OneOf([
+                GridDistortion(p=0.5),
+                OpticalDistortion(p=0.5, distort_limit=2, shift_limit=0.5)
+            ], p=0.5),
+            OneOf([
+                RandomGamma(gamma_limit=(100,140), p=0.5),
+                RandomBrightnessContrast(p=0.5),
+                RandomBrightness(p=0.5),
+                RandomContrast(p=0.5)
+            ], p=0.5),
+            OneOf([
+                GaussNoise(p=0.5),
+                Cutout(num_holes=10, max_h_size=10, max_w_size=20, p=0.5)
+            ], p=0.5),
+            ShiftScaleRotate(rotate_limit=20, p=0.5),
+        ])
+        val_augmentation = None
+
+        train_dataset = SeverDataset(df, IMG_DIR, IMG_SIZE, N_CLASSES, id_colname=ID_COLUMNS,
+                                    transforms=train_augmentation, crop_rate=1.0, class_y=y)
+        train_sampler = MaskProbSampler(df, demand_non_empty_proba=0.6)
+        train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, sampler=train_sampler, num_workers=8)
+
+        del df, train_dataset
+        gc.collect()
+
+    with timer('create model'):
+        model = smp.FPN('se_resnext101_32x4d', encoder_weights='imagenet', classes=N_CLASSES, encoder_se_module=True)
+        model = convert_model(model)
+        if base_model is not None:
+            model.load_state_dict(torch.load(base_model))
+        model.to(device)
+
+        criterion = torch.nn.BCEWithLogitsLoss()
+        optimizer = torch.optim.Adam([
+            {'params': model.decoder.parameters(), 'lr': 3e-4},
+            {'params': model.encoder.parameters(), 'lr': 3e-5},
+        ])
+
+
+        model, optimizer = amp.initialize(model, optimizer, opt_level="O1", verbosity=0)
+        model = torch.nn.DataParallel(model)
+
+    with timer('train'):
+        for epoch in range(1, EPOCHS + 1):
+            seed = seed + epoch
+            seed_torch(seed)
+
+            LOGGER.info("Starting {} epoch...".format(epoch))
+            tr_loss = train_one_epoch(model, train_loader, criterion, optimizer, device, cutmix_prob=0.0,
+                                      classification=CLASSIFICATION)
+            LOGGER.info('Mean train loss: {}'.format(round(tr_loss, 5)))
+
+            if epoch % (CLR_CYCLE * 2) == CLR_CYCLE * 2 - 1:
+                torch.save(model.module.state_dict(), 'models/{}_latest.pth'.format(EXP_ID))
+
+            gc.collect()
+
+
+if __name__ == '__main__':
+    main(SEED)
