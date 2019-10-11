a25482580a41e27b1b61e8732d53480d398ff188
okotaku <to78314910@gmail.com>
2019-10-11 22:33:32 +0900

    add exp90

--- exp/exp90_fpn_seres.py
+++ exp/exp90_fpn_seres.py
@@ -25,6 +25,7 @@ from trainer import train_one_epoch, validate
 from scheduler import GradualWarmupScheduler
 sys.path.append("../")
 import segmentation_models_pytorch as smp
+import segmentation_models_pytorch2 as smp_old
 from sync_batchnorm import convert_model
 
 
@@ -45,13 +46,15 @@ SEED = np.random.randint(100000)
 device = "cuda:0"
 IMG_SIZE = (1600, 256)
 CLR_CYCLE = 3
-BATCH_SIZE = 16
-EPOCHS = 100
-EXP_ID = "exp90_fpn_seresnext_all"
-CLASSIFICATION = False
+BATCH_SIZE = 32
+EPOCHS = 101
+FOLD_ID = 2
+EXP_ID = "exp90_fpn_seres"
+CLASSIFICATION = True
 base_ckpt = 0
 base_model = None
-#base_model = "models/{}_latest.pth".format(EXP_ID)
+#base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
+PSEUDO_PATH = "../input/severstal_pseudo02.csv"
 
 setup_logger(out_file=LOGGER_PATH)
 seed_torch(SEED)
@@ -68,9 +71,16 @@ def timer(name):
 def main(seed):
     with timer('load data'):
         df = pd.read_csv(FOLD_PATH)
+        df = df.append(pd.read_csv(PSEUDO_PATH)).reset_index(drop=True)
+        for c in ["EncodedPixels_1", "EncodedPixels_2", "EncodedPixels_3", "EncodedPixels_4"]:
+            df[c] = df[c].astype(str)
+        df["fold_id"] = df["fold_id"].fillna(FOLD_ID+1)
         y = (df.sum_target != 0).astype("float32").values
 
     with timer('preprocessing'):
+        train_df, val_df = df[df.fold_id != FOLD_ID], df[df.fold_id == FOLD_ID]
+        y_train, y_val = y[df.fold_id != FOLD_ID], y[df.fold_id == FOLD_ID]
+
         train_augmentation = Compose([
             Flip(p=0.5),
             OneOf([
@@ -80,23 +90,23 @@ def main(seed):
             OneOf([
                 RandomGamma(gamma_limit=(100,140), p=0.5),
                 RandomBrightnessContrast(p=0.5),
-                RandomBrightness(p=0.5),
-                RandomContrast(p=0.5)
             ], p=0.5),
             OneOf([
                 GaussNoise(p=0.5),
-                Cutout(num_holes=10, max_h_size=10, max_w_size=20, p=0.5)
             ], p=0.5),
             ShiftScaleRotate(rotate_limit=20, p=0.5),
         ])
         val_augmentation = None
 
-        train_dataset = SeverDataset(df, IMG_DIR, IMG_SIZE, N_CLASSES, id_colname=ID_COLUMNS,
-                                    transforms=train_augmentation, crop_rate=1.0, class_y=y)
-        train_sampler = MaskProbSampler(df, demand_non_empty_proba=0.6)
+        train_dataset = SeverDataset(train_df, IMG_DIR, IMG_SIZE, N_CLASSES, id_colname=ID_COLUMNS,
+                                    transforms=train_augmentation, crop_rate=1.0, class_y=y_train)
+        val_dataset = SeverDataset(val_df, IMG_DIR, IMG_SIZE, N_CLASSES, id_colname=ID_COLUMNS,
+                                  transforms=val_augmentation)
+        train_sampler = MaskProbSampler(train_df, demand_non_empty_proba=0.6)
         train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, sampler=train_sampler, num_workers=8)
+        val_loader = DataLoader(val_dataset, batch_size=BATCH_SIZE, shuffle=False, num_workers=8)
 
-        del df, train_dataset
+        del train_df, val_df, df, train_dataset, val_dataset
         gc.collect()
 
     with timer('create model'):
@@ -111,25 +121,63 @@ def main(seed):
             {'params': model.decoder.parameters(), 'lr': 3e-4},
             {'params': model.encoder.parameters(), 'lr': 3e-5},
         ])
+        if base_model is None:
+            scheduler_cosine = CosineAnnealingLR(optimizer, T_max=CLR_CYCLE, eta_min=0.0)
+            scheduler = GradualWarmupScheduler(optimizer, multiplier=1.1, total_epoch=CLR_CYCLE*2, after_scheduler=scheduler_cosine)
+        else:
+            scheduler = CosineAnnealingLR(optimizer, T_max=CLR_CYCLE, eta_min=0.0)
 
-
-        model, optimizer = amp.initialize(model, optimizer, opt_level="O1", verbosity=0)
         model = torch.nn.DataParallel(model)
 
     with timer('train'):
+        train_losses = []
+        valid_losses = []
+
+        best_model_loss = 999
+        best_model_ep = 0
+        checkpoint = base_ckpt+1
+
         for epoch in range(1, EPOCHS + 1):
             seed = seed + epoch
             seed_torch(seed)
 
             LOGGER.info("Starting {} epoch...".format(epoch))
-            tr_loss = train_one_epoch(model, train_loader, criterion, optimizer, device, cutmix_prob=0.0,
+            tr_loss = train_one_epoch_noapex(model, train_loader, criterion, optimizer, device, cutmix_prob=0.0,
                                       classification=CLASSIFICATION)
+            train_losses.append(tr_loss)
             LOGGER.info('Mean train loss: {}'.format(round(tr_loss, 5)))
 
-            if epoch % (CLR_CYCLE * 2) == CLR_CYCLE * 2 - 1:
-                torch.save(model.module.state_dict(), 'models/{}_latest.pth'.format(EXP_ID))
+            valid_loss, val_score = validate(model, val_loader, criterion, device, classification=CLASSIFICATION)
+            valid_losses.append(valid_loss)
+            LOGGER.info('Mean valid loss: {}'.format(round(valid_loss, 5)))
+            LOGGER.info('Mean valid score: {}'.format(round(val_score, 5)))
 
+            scheduler.step()
+
+            if valid_loss < best_model_loss:
+                torch.save(model.module.state_dict(), 'models/{}_fold{}_ckpt{}.pth'.format(EXP_ID, FOLD_ID, checkpoint))
+                best_model_loss = valid_loss
+                best_model_ep = epoch
+                #np.save("val_pred.npy", val_pred)
+
+            if epoch % (CLR_CYCLE * 2) == CLR_CYCLE * 2 - 1:
+                torch.save(model.module.state_dict(), 'models/{}_fold{}_latest.pth'.format(EXP_ID, FOLD_ID))
+                LOGGER.info('Best valid loss: {} on epoch={}'.format(round(best_model_loss, 5), best_model_ep))
+                checkpoint += 1
+                best_model_loss = 999
+
+            #del val_pred
             gc.collect()
+
+    LOGGER.info('Best valid loss: {} on epoch={}'.format(round(best_model_loss, 5), best_model_ep))
+
+    xs = list(range(1, len(train_losses) + 1))
+    plt.plot(xs, train_losses, label='Train loss')
+    plt.plot(xs, valid_losses, label='Val loss')
+    plt.legend()
+    plt.xticks(xs)
+    plt.xlabel('Epochs')
+    plt.savefig("loss.png")
 
 
 if __name__ == '__main__':
