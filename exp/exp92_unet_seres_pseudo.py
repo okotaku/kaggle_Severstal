d9808504815106be741bcc41ec73e1b142352086
okotaku <to78314910@gmail.com>
2019-10-13 18:28:15 +0900

    add exp92

--- exp/exp92_unet_seres_pseudo.py
+++ exp/exp92_unet_seres_pseudo.py
@@ -51,9 +51,9 @@ EPOCHS = 101
 FOLD_ID = 1
 EXP_ID = "exp92_unet_seresnext"
 CLASSIFICATION = True
-base_ckpt = 0
+base_ckpt = 2
 base_model = None
-#base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
+base_model = "models/{}_fold{}_latest.pth".format(EXP_ID, FOLD_ID)
 PSEUDO_PATH = "../input/severstal_pseudo02.csv"
 
 setup_logger(out_file=LOGGER_PATH)
@@ -143,7 +143,7 @@ def main(seed):
         best_model_ep = 0
         checkpoint = base_ckpt+1
 
-        for epoch in range(1, EPOCHS + 1):
+        for epoch in range(12, EPOCHS + 1):
             seed = seed + epoch
             seed_torch(seed)
 
